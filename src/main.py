d4cf2dcb014c984cc76298dc43002ef6ba4bf5e5
Filippo Pedrini <148688485+filippopedrini95@users.noreply.github.com>
2026-03-12 02:56:42 +0000

    checkpoint

--- src/main.py
+++ src/main.py
@@ -10,6 +10,4 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MODELS_DIR = BASE_DIR / "models"
 model_path = MODELS_DIR / "imgclf_resnet50_cifar10_v1.keras"
 
-t = 'cacca'
-
-print(Fore.BROWN + f"\nmodel saved at: {model_path}", t + Style.RESET_ALL) 
+print(Fore.MAGENTA + f"\ncheck" + Style.RESET_ALL) 
